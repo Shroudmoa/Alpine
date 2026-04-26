2f7c46a13425489dcc01351c52fcf2e1bb0f9838
Shroudmoa <mikeshroud10@gmail.com>
2026-04-26 20:57:24 +0200

    Update installer-i3.sh

--- installer-i3.sh
+++ installer-i3.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-# ==== AUTO-DETECT USER ====
+
 USER_NAME="${SUDO_USER:-$(whoami)}"
 HOME_DIR=$(eval echo "~$USER_NAME")
 MIRROR="http://dl-cdn.alpinelinux.org/alpine"
 
 echo "[+] Using user: $USER_NAME"
 
-# ==== REPOSITORIES ====
+
 echo "[+] Setting repositories to edge..."
 doas sh -c "cat > /etc/apk/repositories <<EOF
 $MIRROR/edge/main
@@ -15,17 +15,16 @@ $MIRROR/edge/community
 $MIRROR/edge/testing
 EOF"
 
-# ==== DOAS CONFIG ====
+
 echo "[+] Configuring doas (nopass)..."
 doas mkdir -p /etc/doas.d
 echo "permit nopass $USER_NAME as root" | doas tee /etc/doas.d/doas.conf
 
-# ==== SYSTEM UPDATE ====
+
 echo "[+] Updating system..."
 doas apk update
 doas apk upgrade
 
-# ==== XORG + I3 ====
 echo "[+] Installing Xorg..."
 doas setup-xorg-base
 
@@ -36,43 +35,44 @@ doas apk add \
     font-dejavu adwaita-icon-theme \
     chromium thunar flameshot
 
-# ==== GROUPS ====
+
 echo "[+] Adding user to input group..."
 doas adduser $USER_NAME input
 
-# ==== LIGHTDM ====
+
 echo "[+] Installing LightDM..."
 doas apk add lightdm lightdm-gtk-greeter
 doas rc-update add lightdm
 
-# ==== PICOM ====
+
 echo "[+] Installing Picom..."
 doas apk add picom
 mkdir -p "$HOME_DIR/.config/picom"
 doas cp /etc/xdg/picom.conf.example "$HOME_DIR/.config/picom/picom.conf"
 doas chown -R "$USER_NAME:$USER_NAME" "$HOME_DIR/.config"
 
-# ==== DEV TOOLS ====
+
 echo "[+] Installing developer tools..."
 doas apk add \
     git make libx11-dev libxinerama-dev \
     libxft-dev g++ fontconfig-dev \
     ncurses curl
 
-# ==== SERVICES ====
+
 echo "[+] Enabling dbus..."
 doas rc-update add dbus
 doas rc-service dbus start
 
-# ==== THEMES ====
+
 echo "[+] Installing GTK tools and themes..."
 doas apk add lxappearance mate-themes papirus-icon-theme capitaine-cursors
+doas apk add alacritty librewolf
 
-# ==== AUDIO ====
+
 echo "[+] Installing ALSA..."
 doas apk add alsa-utils
 
-# ==== XINIT ====
+
 echo "[+] Creating .xinitrc..."
 echo "exec i3" > "$HOME_DIR/.xinitrc"
 doas chown "$USER_NAME:$USER_NAME" "$HOME_DIR/.xinitrc"
